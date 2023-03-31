20454f4ea4e28c4504d9ed2b5c1b94eea569d102
Carlo Nicora <carlo.nicora@gmail.com>
2023-03-31 21:36:02 +0200

    fix: specialise findChild for ServiceData

--- src/Data/ServiceData.php
+++ src/Data/ServiceData.php
@@ -3,10 +3,25 @@ namespace CarloNicora\Minimalism\Services\Discovery\Data;
 
 use CarloNicora\Minimalism\Services\Discovery\Data\Abstracts\AbstractDataList;
 use CarloNicora\Minimalism\Services\Discovery\Data\enums\DataTypes;
+use CarloNicora\Minimalism\Services\Discovery\Interfaces\DataInterface;
+use CarloNicora\Minimalism\Services\Discovery\Interfaces\DataListInterface;
 
 class ServiceData extends AbstractDataList
 {
+    /**
+     * @param string $id
+     * @return DataInterface|DataListInterface|null
+     */
+    public function findChild(string $id): DataInterface|DataListInterface|null
+    {
+        foreach ($this->children as $child){
+            if (strtolower($child->getId()) === strtolower($id)){
+                return $child;
+            }
+        }
 
+        return null;
+    }
     /**
      * @return DataTypes
      */
